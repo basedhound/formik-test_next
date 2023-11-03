7e5e3a04aea078fa0c0cc71ca6bffbbcbfadc2a4
Jozef S <franck.vukelic@gmail.com>
2023-11-03 17:48:00 +0700

    fix

--- pages/success.js
+++ pages/success.js
@@ -21,8 +21,7 @@ export default function Success() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className=" h-screen items-center flex justify-center relative"
-    >
+      className=" h-screen items-center flex justify-center relative">
       <div className="bg-white rounded-lg w-1/3 font-latoRegular text-gray-700 p-16">
         <h1 className="text-3xl pb-4 font-latoBold">
           Thanks for the email {router.query.name} ✨
